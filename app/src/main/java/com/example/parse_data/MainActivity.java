416cbc5c280691e5f59ad857a420b8a0b4c7dee3
farihanoor <Fariha041295**>
2021-08-08 21:02:19 +0600

    configured volley package

--- app/src/main/java/com/example/parse_data/MainActivity.java
+++ app/src/main/java/com/example/parse_data/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.parse_data;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+    // The RequestQueue is the object that will make sure
+    // that all these transactions. So, if we fetch data from the
+    // internet or somewhere else, it will QUEUE all these threads
+    // (transactions) in the background.
+
+
+    String url = "https://www.google.com";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                // display the contents of the url
+                Log.d("Main", "OnCreate: " + response.substring(0,500));
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Main", "Failed to get info!");
+            }
+        });
+
+        // add the request to the RequestQueue
+        queue.add(stringRequest);
     }
 }
